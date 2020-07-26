51e9248f5e153c1271fcb31d9537ee815169ada0
Dustin B <dustinbeuerlein1@gmail.com>
2020-07-26 14:06:51 -0400

    Update xoce-import.sh
    Compressed XVA

--- xoce-import.sh
+++ xoce-import.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-XOCE_URL=https://nc.nl.tab.digital/s/GoQx6xJQQy3GXnA/download
+XOCE_URL=https://nc.nl.tab.digital/s/eE2A5yJgLQ4Gpo3/download
 
 # Welcome message
 printf "\n\033[1mWelcome to the XOCE auto-deploy script!\033[0m\n\n"
