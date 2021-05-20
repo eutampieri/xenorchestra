4ed35c8fa3d6d1348106e6ce9b644f31e45d56f7
Dan Pollak <danpollak2@gmail.com>
2021-05-20 11:01:59 -0500

    Fix: Update launched module (#94)

--- xo_install.sh
+++ xo_install.sh
@@ -82,7 +82,8 @@ After=network-online.target
 
 [Service]
 WorkingDirectory=/opt/xen-orchestra/packages/xo-server/
-ExecStart=/usr/local/bin/node ./bin/xo-server
+ExecStart=/usr/local/bin/node ./dist/cli.mjs
+
 Restart=always
 SyslogIdentifier=xo-server
 
